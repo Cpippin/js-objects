17dbda36d9a4a8686e50a928a25cb8f1e9f1153e
Christian Pippin <ianpippin@gmail.com>
2019-10-09 14:42:19 -0500

    added js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 console.log("we're learning about objects")
+
+const car = {
+    make: "ford",
+    model: "mustang",
+    color: "red",
+    year: "2015"
+}
+
+let names = [ "kippers", "jack", "gypsy", "angus", "seymour bouts", "sharky" ]
+
+const amy = {
+    relation: "mother",
+    age: "40",
+    gender: "female"
+}
+
+const joey = {
+    relation: "father",
+    age: "42",
+    gender: "male"
+}
+
+const joanna = {
+    relation: "sister",
+    age: "19",
+    gender: "female"
+}
+
+const brant = {
+    relation: "brother",
+    age: "17",
+    gender: "male"
+}
+
+let familyMembers = [ amy, joey, joanna, brant ]
+
+console.log(car)
+console.log(names)
+console.log(familyMembers)
